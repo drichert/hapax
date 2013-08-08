fed416f12853780edb7523cca8a71f6c8cf93860
Dan Richert <dan.richert@gmail.com>
2013-08-07 22:58:21 -0400

    Add rspec rake tasks, spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require "rspec"
+require "bundler/setup"
+
+require "sep"
+
+module RSpec::Hapax
+  def self.text
+    path = File.expand_path("../support/text", __FILE__)
+
+    File.read(path)
+  end
+end
+
+RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+  c.color_enabled = true
+end
